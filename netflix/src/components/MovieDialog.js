cc3c332d918c1b03df295791306bc19365f1cf4c
Vibhor <3646vibes@gmail.com>
2026-07-14 02:04:45 +0530

    Fix Vercel build warnings

--- netflix/src/components/MovieDialog.js
+++ netflix/src/components/MovieDialog.js
@@ -1,25 +1,23 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import {useSelector,useDispatch} from "react-redux";
-import { setOpen } from '../redux/movieSlice';
-import VideoBackground from './VideoBackground';
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import { useSelector, useDispatch } from "react-redux";
+import { setOpen } from "../redux/movieSlice";
+import VideoBackground from "./VideoBackground";
 
-export default function MovieDialog() { 
-  const {open,id} = useSelector(store=>store.movie);
+export default function MovieDialog() {
+  const { open, id } = useSelector((store) => store.movie);
   const dispatch = useDispatch();
 
-  const handleClose = ()=>{
+  const handleClose = () => {
     dispatch(setOpen(false));
-  }
- 
+  };
+
   return (
     <React.Fragment>
-      
       <Dialog
         open={open}
         onClose={handleClose}
@@ -29,21 +27,23 @@ export default function MovieDialog() {
         fullWidth
         PaperProps={{
           sx: {
-            backgroundColor: '#181818',
-            borderRadius: '8px',
-            overflow: 'hidden',
-          }
+            backgroundColor: "#181818",
+            borderRadius: "8px",
+            overflow: "hidden",
+          },
         }}
       >
-       <DialogContent sx={{ padding: 0 }}>
+        <DialogContent sx={{ padding: 0 }}>
           <DialogContentText id="alert-dialog-description" component="div">
-            <VideoBackground movieId={id} bool = {true}/>
+            <VideoBackground movieId={id} bool={true} />
           </DialogContentText>
         </DialogContent>
-        <DialogActions sx={{ backgroundColor: '#181818' }}>
-          <Button onClick={handleClose} sx={{ color: '#fff' }}>Cancel</Button>
+        <DialogActions sx={{ backgroundColor: "#181818" }}>
+          <Button onClick={handleClose} sx={{ color: "#fff" }}>
+            Cancel
+          </Button>
         </DialogActions>
       </Dialog>
     </React.Fragment>
   );
-}
+}
--- netflix/src/hooks/useMovieById.js
+++ netflix/src/hooks/useMovieById.js
@@ -1,23 +1,29 @@
 import axios from "axios";
-import { options } from '../utils/constant';
+import { options } from "../utils/constant";
 import { useDispatch } from "react-redux";
-import { getTrailerMovie, setHeroTrailerMovie } from '../redux/movieSlice';
+import { getTrailerMovie, setHeroTrailerMovie } from "../redux/movieSlice";
 import { useEffect } from "react";
-
 
 const useMovieById = (movieId, isHero = false) => {
   const dispatch = useDispatch();
-  
+
   useEffect(() => {
     if (!movieId) return;
+
     const getMovieById = async () => {
       try {
-        const res = await axios.get(`https://api.themoviedb.org/3/movie/${movieId}/videos`, options);
+        const res = await axios.get(
+          `https://api.themoviedb.org/3/movie/${movieId}/videos`,
+          options,
+        );
 
-        const trailer = res?.data?.results?.filter((item) => {
-          return item.type === "Trailer";
-        })
-        const trailerData = trailer.length > 0 ? trailer[0] : res.data.results[0];
+        const trailer = res?.data?.results?.filter(
+          (item) => item.type === "Trailer",
+        );
+
+        const trailerData =
+          trailer.length > 0 ? trailer[0] : res.data.results[0];
+
         if (isHero) {
           dispatch(setHeroTrailerMovie(trailerData));
         } else {
@@ -26,10 +32,10 @@ const useMovieById = (movieId, isHero = false) => {
       } catch (error) {
         console.log(error);
       }
-    }
+    };
+
     getMovieById();
-  },[movieId])
+  }, [movieId, dispatch, isHero]);
+};
 
-}
-
-export default useMovieById;
+export default useMovieById;
